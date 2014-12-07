ceac3fddc57f85688e9f1b7b523b8ae10c964cf7
bvernoux <bvernoux@gmail.com>
2014-12-07 17:42:34 +0100

    Modified airspy lib version to 1.0.3

--- libairspy/src/airspy.h
+++ libairspy/src/airspy.h
@@ -28,10 +28,10 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSI
 #include <stdint.h>
 #include "airspy_commands.h"
 
-#define AIRSPY_VERSION "1.0.2"
+#define AIRSPY_VERSION "1.0.3"
 #define AIRSPY_VER_MAJOR 1
 #define AIRSPY_VER_MINOR 0
-#define AIRSPY_VER_REVISION 2
+#define AIRSPY_VER_REVISION 3
 
 #ifdef _WIN32
 	 #define ADD_EXPORTS
